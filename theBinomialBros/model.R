6a06220ad5bd72705049e6f9d703e717030c01ad
euaneac <56359608+euaneac@users.noreply.github.com>
2020-03-16 16:29:16 +0000

    Uploading the R project

---
+++ theBinomialBros/model.R
@@ -0,0 +1,6 @@
+#The binimial bros course work
+#Working out the profit of a bus journey
+#profit = ticket sales - running cost - lost revenue
+#ticket sales = (child numbers * child ticket price) + (student number * student ticket price) + (adult number * adult ticket price)  + (senior number * senior ticket price)
+#running cost = (journey length * cost per mile) + (wages)
+#lost revenue = 
